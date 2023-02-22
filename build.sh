d3aa0895c2f20f864ab8e0fb6b58a1f6960e0d3b
Will Toth <wtoth1631@gmail.com>
2023-02-21 21:02:10 -0600

    How do you enable jstatd to build?

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ set -o pipefail
 source versions.sh
 
 JVM_VARIANT=client
+#JVM_VARIANT=minimal
+#JVM_FEATURES=jvmti,services,vm-structs
 JVM_FEATURES=
 #JVM_VARIANT=minimal1
-#JVM_FEATURES=all-gcs,jvmti,services,vm-structs
+#JVM_FEATURES=jvmti,services,vm-structs
 
 wget -nc https://github.com/openjdk/jdk17u/archive/refs/tags/${GIT_TAG}.tar.gz
 tar xzf ${GIT_TAG}.tar.gz
@@ -25,29 +27,29 @@ bash configure \
 	--with-version-patch=${JAVA_PATCH} \
 	--with-version-opt=${YEAR}-${VER} \
 	--disable-warnings-as-errors
-make JOBS=`nproc` LOG=cmdlines all legacy-jre-image
+make JOBS=`nproc` LOG=cmdlines all images
 pushd build/linux-arm-${JVM_VARIANT}-release/images
-tar czf jre_${VER}.tar.gz jre
-chown -R `id -u`:`id -g` jre_${VER}.tar.gz
-cp -a jre_${VER}.tar.gz /artifacts
-find jre -name \*.diz -delete
-find jre -name \*.so -type f | xargs arm-frc${YEAR}-linux-gnueabi-strip
-arm-frc${YEAR}-linux-gnueabi-strip jre/bin/* jre/lib/jexec
-tar czf jre_${VER}-strip.tar.gz jre
-chown -R `id -u`:`id -g` jre_${VER}-strip.tar.gz
-cp -a jre_${VER}-strip.tar.gz /artifacts
+tar czf jdk_${VER}.tar.gz jdk
+chown -R `id -u`:`id -g` jdk_${VER}.tar.gz
+cp -a jdk_${VER}.tar.gz /artifacts
+find jdk -name \*.diz -delete
+find jdk -name \*.so -type f | xargs arm-frc${YEAR}-linux-gnueabi-strip
+arm-frc${YEAR}-linux-gnueabi-strip jdk/bin/* jdk/lib/jexec
+tar czf jdk_${VER}-strip.tar.gz jdk
+chown -R `id -u`:`id -g` jdk_${VER}-strip.tar.gz
+cp -a jdk_${VER}-strip.tar.gz /artifacts
 popd
 popd
 
 rm -f control.tar.gz data.tar.gz
-rm -rf jre
+rm -rf jdk
 
-tar xzf /artifacts/jre_${VER}-strip.tar.gz
+tar xzf /artifacts/jdk_${VER}-strip.tar.gz
 tar czf data.tar.gz \
-    --transform "s,^jre,usr/local/frc/JRE," \
+    --transform "s,^jdk,usr/local/frc/JDK," \
     --owner=root \
     --group=root \
-    jre
+    jdk
 tar czf control.tar.gz control postinst prerm
 echo 2.0 > debian-binary
 ar r /artifacts/${IPK_NAME} control.tar.gz data.tar.gz debian-binary
